52b5aee185a1f87ffbdaacc2eb998c9cd8ca43f0
Malazzano <isaack.souzaz@gmail.com>
2021-04-23 14:05:11 -0300

    Adiciona HomePage

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,12 +1,11 @@
-import 'package:DevQuiz/home/home_page.dart';
-import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:zanoquiz/home/home_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "DevQuiz",
+      title: "ZanoQuiz",
       home: HomePage(),
     );
   }
