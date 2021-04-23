300fc3d5c09a447002163415d9bce1a22bfe0975
Malazzano <isaack.souzaz@gmail.com>
2021-04-23 16:10:07 -0300

    Adicionar Splash

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:zanoquiz/home/home_page.dart';
+import 'package:zanoquiz/splash/splach_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
